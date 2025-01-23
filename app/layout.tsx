c54e590ac2bd0f4b0b4e5a3070c3ac87e9127f32
jefersonwork <jefersoncosta336@gmail.com>
2025-01-22 21:11:35 -0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body
+        className={`antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
