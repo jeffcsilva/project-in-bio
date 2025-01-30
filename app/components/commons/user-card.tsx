6822b7e46b4e2ae4b04dbc55f4eb2adf1f6aae40
jefersonwork <jefersoncosta336@gmail.com>
2025-01-29 22:04:33 -0300

    tortal visits components

--- app/components/commons/user-card.tsx
+++ app/components/commons/user-card.tsx
@@ -38,13 +38,13 @@ export default function UserCard() {
             ))
           }
         </div>
-        <div className="flex flex-col gap-4 w-full h-[172px]">
-          <div className="w-full flex flex-col items-center gap-3">
-            <Button className="w-full">Template SaaS - Compre agora</Button>
-            <button className="p-3 rounded-xl bg-[#1e1e1e] hover:bg-[#2a2a2a]">
-              <Plus />
-            </button>
-          </div>
+      </div>
+      <div className="flex flex-col gap-4 w-full h-[172px]">
+        <div className="w-full flex flex-col items-center gap-3">
+          <Button className="w-full">Template SaaS - Compre agora</Button>
+          <button className="p-3 rounded-xl bg-[#1e1e1e] hover:bg-[#2a2a2a]">
+            <Plus />
+          </button>
         </div>
       </div>
     </div>
